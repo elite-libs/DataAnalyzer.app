7ebfc8c079a1559644cf13ee67afa0fb9555e10c
Dan Levy <dan@danlevy.net>
2021-02-28 14:32:15 -0700

    WIP: consolidating types, progress!!

--- src/schema-analyzer/__tests__/nested-objects.test.tsx
+++ src/schema-analyzer/__tests__/nested-objects.test.tsx
@@ -1,8 +1,13 @@
-import { schemaAnalyzer } from '../';
+import {
+  ISchemaAnalyzerOptions,
+  schemaAnalyzer,
+  helpers,
+  consolidateNestedTypes,
+} from '../';
 import historicEvents from '../../../public/data/historic-events.json';
 // import pokemonCharmander from '../../../public/data/pokemon-charmander.json';
 
-describe('#nested non-array object types', () => {
+describe('#nested objects', () => {
   // Uses data from http://history.muffinlabs.com/date/2/16
   it('can parse nested objects (non-array)', async () => {
     try {
@@ -12,12 +17,70 @@ describe('#nested non-array object types', () => {
       expect(Object.keys(result.fields)).toStrictEqual(['date', 'url', 'data']);
       expect(result.nestedTypes).not.toBeNull();
       expect(result.nestedTypes?.['historicEvent.data']).toBeDefined();
+      expect(Object.keys(result.nestedTypes)).toStrictEqual([
+        'historicEvent.data',
+        'historicEvent.data.Events',
+        'historicEvent.data.Births',
+        'historicEvent.data.Deaths',
+        'historicEvent.data.Events.links',
+        'historicEvent.data.Births.links',
+        'historicEvent.data.Deaths.links',
+      ]);
       expect(
         Object.keys(result.nestedTypes?.['historicEvent.data']?.fields!),
       ).toStrictEqual(['Events', 'Births', 'Deaths']);
     } catch (error) {
       console.error('ERROR:', error);
+      throw error;
     }
+  });
+  describe('#de-duplicating subtypes', () => {
+    // Uses data from http://history.muffinlabs.com/date/2/16
+    it('can match types by their field names', async () => {
+      try {
+        const options: ISchemaAnalyzerOptions = {
+          strictMatching: false,
+          consolidateTypes: 'field-names',
+        };
+
+        const summary = await schemaAnalyzer(
+          'historicEvent',
+          historicEvents,
+          options,
+        );
+        const result = helpers.flattenTypes(summary, {
+          nullableRowsThreshold: 0.0001,
+          targetValue: 'p99',
+          targetLength: 'p99',
+          targetPrecision: 'p99',
+          targetScale: 'p99',
+        });
+        expect(
+          consolidateNestedTypes(result.nestedTypes, options),
+        ).toBeDefined();
+        // console.log(JSON.stringify(result, null, 2));
+        // expect(result).toMatchSnapshot();
+        expect(Object.keys(result.fields)).toStrictEqual([
+          'date',
+          'url',
+          'data',
+        ]);
+        expect(result.nestedTypes).not.toBeNull();
+        expect(Object.keys(result.nestedTypes)).toStrictEqual([
+          'historicEvent.data',
+          'historicEvent.data.Events',
+          'historicEvent.data.Births',
+          'historicEvent.data.Deaths',
+          'links',
+        ]);
+        expect(result.nestedTypes?.['historicEvent.data']).toBeDefined();
+        expect(
+          Object.keys(result.nestedTypes?.['historicEvent.data']?.fields!),
+        ).toStrictEqual(['Events', 'Births', 'Deaths']);
+      } catch (error) {
+        console.error('ERROR:', error);
+      }
+    });
   });
   // https://pokeapi.co/api/v2/pokemon/4
   // pokemonCharmander
