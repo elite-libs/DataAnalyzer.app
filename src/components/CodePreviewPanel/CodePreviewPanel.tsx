5d77bb885a700c9a19fc435f54abf743fb99080a
Dan Levy <dan@danlevy.net>
2021-05-03 00:13:54 -0600

    checking esm bug

--- src/components/CodePreviewPanel/CodePreviewPanel.tsx
+++ src/components/CodePreviewPanel/CodePreviewPanel.tsx
@@ -1,10 +1,10 @@
 import React, { ReactNode } from 'react';
 import { useSelector } from 'react-redux';
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
 import ts from 'react-syntax-highlighter/dist/esm/languages/prism/typescript';
 import golang from 'react-syntax-highlighter/dist/esm/languages/prism/go';
 import sql from 'react-syntax-highlighter/dist/esm/languages/prism/sql';
+import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
 
 import ghcolors from 'react-syntax-highlighter/dist/esm/styles/prism/ghcolors';
 // import { ghcolors } from 'react-syntax-highlighter/dist/cjs/styles/prism';
