da45e98395b7106ae9c4497bc162e36fca65433e
Dan Levy <dan@danlevy.net>
2021-05-03 01:13:15 -0600

    better onProgress event frequency

--- src/schema-analyzer/index.tsx
+++ src/schema-analyzer/index.tsx
@@ -353,7 +353,7 @@ const _pivotRowsGroupedByType = ({
 
       const totalRows = schema.totalRows;
       const isDone = index + 1 === totalRows;
-      const progressFrequencyModulo = 10;
+      const progressFrequencyModulo = 100;
       // totalRows >= 2500 ? 20 : totalRows >= 1000 ? 25 : 10;
       const showProgress = !isDone && index % progressFrequencyModulo === 0;
 
