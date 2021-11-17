9f87fd98141eaed249cf9abfe771c5983620a0ed
Dan Levy <dan@danlevy.net>
2021-11-17 14:12:18 -0700

    Working Zod generator!

--- src/adapters/writer.zod.ts
+++ src/adapters/writer.zod.ts
@@ -1,6 +1,8 @@
 import camelCase from 'lodash/camelCase';
+import trim from 'lodash/trim';
 import {
   CombinedFieldInfo,
+  FieldTypeSummary,
   NumericFieldInfo,
   TypeNameStringComposite,
   TypeNameStringDecimal,
@@ -15,14 +17,14 @@ const getZodType = ({ type, ...data }: CombinedFieldInfo) => {
   if (type === '$ref') return `object()`;
   if (type === 'Unknown') return `string()`;
   if (type === 'ObjectId') return `string()`;
-  if (type === 'UUID') return `string().uuid())`;
+  if (type === 'UUID') return `string().uuid()`;
   if (type === 'Boolean') return `boolean()`;
   if (type === 'Date') return `date()`;
   if (type === 'Timestamp') return `date()`;
   if (type === 'Currency') return `number()`;
   if (type === 'Float') return `number()`;
   if (type === 'Number') return `number()`;
-  if (type === 'BigNumber') return `string()`;
+  if (type === 'BigNumber') return `bigint()`;
   if (type === 'Email') return `string().email()`;
   if (type === 'String') return `string()`;
   if (type === 'Array') return `array()`;
@@ -32,24 +34,61 @@ const getZodType = ({ type, ...data }: CombinedFieldInfo) => {
   return `any()`;
 };
 const writer: IDataAnalyzerWriter = {
-  render(results) {
+  render(results, { MIN_ROWS_FOR_TYPE_CONSTRAINTS = 10 } = {}) {
     const { options } = results;
 
     const typeSummary = results.flatTypeSummary;
     const hasNestedTypes =
       typeSummary.nestedTypes && Object.keys(typeSummary.nestedTypes!).length > 0;
     // const { fields } = typeSummary;
+    const enableConstraints = results.totalRows >= MIN_ROWS_FOR_TYPE_CONSTRAINTS;
+
     const getSchema = (schemaName: string, fields: KeyValPair<CombinedFieldInfo>) => {
       return (
-        `\nexport const ${properCase(schemaName)}Schema = z.object({\n` +
+        `export type ${properCase(schemaName)} = z.infer<typeof ${properCase(
+          schemaName,
+        )}Schema>;
+export const ${properCase(schemaName)}Schema = z.object({\n` +
         Object.entries(fields)
           .map(([fieldName, fieldInfo]) => {
+            const normalizedRefName = trim(fieldInfo.typeRef, '_');
             const refProperName =
-              typeof fieldInfo.typeRef === 'string'
-                ? fieldInfo.typeRef![0]?.toUpperCase() +
-                  camelCase(fieldInfo.typeRef).slice(1)
+              typeof normalizedRefName === 'string'
+                ? normalizedRefName![0]?.toUpperCase() +
+                  camelCase(normalizedRefName).slice(1)
                 : undefined;
-            // fieldInfo.typeSummary.value
+
+            console.log(fieldName, fieldInfo.typeRef, refProperName, normalizedRefName);
+
+            const {
+              type,
+              typeSummary: {
+                value: valueAggregate,
+                length: lengthAggregate,
+                // precision: precisionAggregate,
+                // scale: scaleAggregate,
+              },
+            } = fieldInfo;
+            const bigNumSuffix = type === 'BigNumber' ? 'n' : '';
+            const maxValue =
+              typeof valueAggregate === 'object' && typeof valueAggregate.max === 'number'
+                ? valueAggregate.max
+                : undefined;
+            const minValue =
+              typeof valueAggregate === 'object' && typeof valueAggregate.min === 'number'
+                ? valueAggregate.min
+                : undefined;
+            const maxLength =
+              typeof lengthAggregate === 'object' &&
+              typeof lengthAggregate.max === 'number'
+                ? lengthAggregate.max
+                : undefined;
+            const minLength =
+              typeof lengthAggregate === 'object' &&
+              typeof lengthAggregate.min === 'number'
+                ? lengthAggregate.min
+                : undefined;
+
             if (fieldInfo == null) return `// null field info !!!`;
             if (fieldInfo.typeRef)
               return `  ${camelCase(fieldName)}: z.lazy(() => ${refProperName}Schema)`;
@@ -57,20 +96,16 @@ const writer: IDataAnalyzerWriter = {
             return `  ${camelCase(fieldName)}: z.${getZodType(fieldInfo)}${
               fieldInfo.nullable ? '' : '.required()'
             }${
-              TypeNameStringDecimal.includes(fieldInfo.type) &&
-              typeof fieldInfo.typeSummary.value === 'object' &&
-              typeof fieldInfo.typeSummary.value.max === 'number'
-                ? '.max(' +
-                  (fieldInfo as NumericFieldInfo).typeSummary.value!['max'] +
-                  ')'
+              enableConstraints && (minValue || minLength)
+                ? `.min(${minValue || minLength}${bigNumSuffix})`
                 : ''
             }${
-              fieldInfo.value && TypeNameStringDecimal.includes(fieldInfo.type)
-                ? '.min(' + (fieldInfo as NumericFieldInfo).value + ')'
+              enableConstraints && (maxValue || maxLength)
+                ? `.max(${maxValue || maxLength}${bigNumSuffix})`
                 : ''
             }${
-              fieldInfo.value && TypeNameStringComposite.includes(fieldInfo.type)
-                ? '.length(' + fieldInfo.value + ')'
+              enableConstraints && minLength && minLength === maxLength
+                ? `.length(${maxLength}, { message: "Exact length required: ${maxLength}" })`
                 : ''
             }${
               Array.isArray(fieldInfo.enum) && fieldInfo.enum.length > 0
@@ -97,30 +132,30 @@ const writer: IDataAnalyzerWriter = {
     let code = moveModuleExports(
       getSchema(results.schemaName!, typeSummary.fields) + getRecursive(),
     );
-    return getHeader() + code;
+    return getHeader() + code + '\n';
   },
 };
 
 function moveModuleExports(code: string): string {
-  const mongooseModelLines = /^const.*mongoose.model.*$/gim;
-  const moduleLines = /^module\.exports.*$/gim;
+  // const mongooseModelLines = /^const.*mongoose.model.*$/gim;
+  const moduleLines = /^export type.*$/gim;
   let moduleMatch: null | string[] = moduleLines.exec(code);
-  let mongooseMatch: null | string[] = mongooseModelLines.exec(code);
-  const moduleConstLines: string[] = [];
+  // let mongooseMatch: null | string[] = mongooseModelLines.exec(code);
+  // const moduleConstLines: string[] = [];
   const moduleExportLines: string[] = [];
   while (Array.isArray(moduleMatch) && moduleMatch.length > 0) {
     moduleExportLines.push(...moduleMatch);
     moduleMatch = moduleLines.exec(code);
   }
-  while (Array.isArray(mongooseMatch) && mongooseMatch.length > 0) {
-    moduleConstLines.push(...mongooseMatch);
-    mongooseMatch = mongooseModelLines.exec(code);
-  }
+  // while (Array.isArray(mongooseMatch) && mongooseMatch.length > 0) {
+  //   moduleConstLines.push(...mongooseMatch);
+  //   mongooseMatch = mongooseModelLines.exec(code);
+  // }
 
   code =
-    code.replace(moduleLines, '').replace(mongooseModelLines, '') +
-    moduleConstLines.join('\n') +
-    '\n\n' +
+    code.replace(moduleLines, '') +
+    // moduleConstLines.join('\n') +
+    '\n' +
     moduleExportLines.join('\n');
   return code;
 }
